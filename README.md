e7422a8e33516aba1c51f36e24e9ea849ffa094a
MikoreVikt <136119613+MrTkachenkoA@users.noreply.github.com>
2023-06-11 21:10:27 +0300

    adds start files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Laboratory work 6
+# Laboratory work 7
--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,14 +1,38 @@
-export const TravelList = ({ routes }) => {
-  const getInitialState = () => {
-    const routeNames = [];
-    routes.map(({ name }) => routeNames.push(name));
-    return routeNames;
+import { useState } from "react";
+
+export const TravelList = (props) => {
+  const [routes, setRoutes] = useState(props.routes);
+
+  console.log(routes);
+
+  const deleteRoute = (id) => {
+    setRoutes((prevRoutes) => prevRoutes.filter((route) => route.id !== id));
   };
 
-  const renderRoutes = () =>
-    getInitialState().map((route, index) => {
-      return <p key={index}>{route}</p>;
+  const editRoute = (id) => {
+    const newName = prompt("Enter the new name for the route:");
+    if (newName) {
+      setRoutes((prevRoutes) =>
+        prevRoutes.map((route) =>
+          route.id === id ? { ...route, name: newName } : route
+        )
+      );
+    }
+  };
+
+  const thetravel = (id, name) => (
+    <div key={id}>
+      <p>{name}</p>
+      <button onClick={() => deleteRoute(id)}>Delete</button>
+      <button onClick={() => editRoute(id)}>Edit</button>
+    </div>
+  );
+
+  const renderRoutes = () => {
+    return routes.map((route) => {
+      return thetravel(route.id, route.name);
     });
+  };
 
   return (
     <div className="tourist-routes_list">
